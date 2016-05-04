c1e1727da1db7dc07d8a0383e056931c1dcebf25
pathFinder <path7inder@gmail.com>
2016-05-04 19:34:56 +0900

    Modified 4 filles     angularApp.js         -deleted all dummy_datas         -add function creating menu
        app.js
            -include Menu schema
            -include Option schema
    
        index.ejs
            -binded name and cost to object varible
    
        index.js
            -added option api interface
            -added menu api interface

--- mcn_coffe/routes/index.js
+++ mcn_coffe/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Cafe = mongoose.model('Cafe');
+var Option = mongoose.model('Option');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,10 +18,10 @@ router.get('/cafes', function(req, res, next){
 });
 
 router.post('/cafes', function(req, res, next) {
-    var post = new Cafe(req.body);
-    post.save(function(err, post){
+    var cafe = new Cafe(req.body);
+    cafe.save(function(err, cafe){
         if(err){ return next(err); }
-        res.json(post);
+        res.json(cafe);
     });
 });
 
@@ -36,6 +37,49 @@ router.param('cafe', function(req, res, next, id) {
 
 router.get('/cafes/:cafe', function(req, res) {
     res.json(req.cafe);
+});
+
+router.get('/options', function(req, res, next){
+    Option.find(function(err, options){
+        if(err){ return next(err); }
+        res.json(options);
+    });
+});
+
+router.post('/options', function(req, res, next){
+    var option = new Option(req.body);
+    option.save(function(err, option){
+        if(err){ return next(err); }
+        res.json(option);
+    });
+});
+
+router.get('/menus', function(req, res, next){
+    Menu.find(function(err, menus){
+        if(err){ return next(err); }
+        res.json(menus);
+    });
+});
+
+router.post('/menus', function(req, res, next){
+    var menu = new Menu(req.body);
+    menu.save(function(err, menu){
+        if(err){ return next(err); }
+    });
+});
+
+router.param('menu', function(req, res, next, id) {
+    var query = Menu.findById(id);
+    query.exec(function (err, menu){
+        if (err) { return next(err); }
+        if (!menu) { return next(new Error('can\'t find cafe')); }
+        req.menu = menu;
+        return next();
+    });
+});
+
+router.get('/cafes/:menu', function(req, res) {
+    res.json(req.menu);
 });
 
 module.exports = router;
