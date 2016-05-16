06d116c0f5d66013a5a727d6c36e7cf0255defbc
pathFinder <path7inder@gmail.com>
2016-05-16 15:54:31 +0900

    Add User Register and Authentcation Controller
    - angularApp.js
        add state 'login' to user login page
        add state 'register' to user register page
        add fatory for auth generating authentication data
        add 'AuthCtrl' as controller for authentication
            trig auth factory register function
            trig auth factory login function
    
    - app.js
        include User model
        include passport module
    
    - index.ejs
        add login.html template
        add register.html tempalte
        link login/register button to login/register page
    
    - index.js
        add register API to post new user account
        add login API to post login process

--- mcn_coffe/app.js
+++ mcn_coffe/app.js
@@ -8,6 +8,8 @@ var mongoose = require('mongoose');
 require('./models/Cafes');
 require('./models/Menus');
 require('./models/Options');
+require('./models/Users');
+require('./config/passport')
 var passport = require('passport');
 
 mongoose.connect('mongodb://localhost/coffee');
@@ -28,9 +30,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- mcn_coffe/public/javascripts/angularApp.js
+++ mcn_coffe/public/javascripts/angularApp.js
@@ -19,6 +19,26 @@ app.config([
                     }]
                 }
             })
+            .state('login', {
+                url: '/login',
+                templateUrl: '/login.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth) {
+                    if( auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/register.html',
+                controller: 'AuthCtrl',
+                onEnter: ['$state', 'auth', function($state, auth) {
+                    if( auth.isLoggedIn()) {
+                        $state.go('home');
+                    }
+                }]
+            })
             .state('registerCafes', {
                 url: '/register/cafes',
                 templateUrl: '/register/cafes.html',
@@ -47,6 +67,87 @@ app.config([
 
         $urlRouterProvider.otherwise('home');
     }]);
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+
+    var auth = {};
+
+    auth.saveToken = function(token){
+        $window.localStorage['flapper-news-token'] = token;
+    };
+
+    auth.getToken = function() {
+        return $window.localStorage['flapper-news-token'];
+    };
+
+    auth.isLoggedIn = function() {
+        var token = auth.getToken();
+
+        if(token) {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.exp = Date.now() / 1000;
+        } else {
+            return false;
+        }
+    };
+
+    auth.currentUser = function() {
+        if(auth.isLoggedIn()) {
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.username;
+        }
+    };
+
+    auth.register = function(user) {
+        return $http.post('/register', user).success(function(data) {
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.logIn = function(user) {
+        return $http.post('/login', user).success(function(data) {
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.logOut = function() {
+        $window.localStorage.removeItem('flapper-news-token');
+    };
+
+    return auth;
+}]);
+
+app.controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'auth',
+    function($scope, $state, auth){
+        $scope.user = {};
+
+        $(document).ready(function() {
+            $.material.init();
+        });
+
+        $scope.register = function(){
+            auth.register($scope.user).error(function(error){
+                $scope.error = error;
+            }).then(function(){
+                $state.go('home');
+            });
+        };
+
+        $scope.logIn = function(){
+            auth.logIn($scope.user).error(function(error){
+                $scope.error = error;
+            }).then(function(){
+                $state.go('home');
+            });
+        }
+    }
+]);
 
 app.factory('cafes', ['$http', function($http){
     var o = {
